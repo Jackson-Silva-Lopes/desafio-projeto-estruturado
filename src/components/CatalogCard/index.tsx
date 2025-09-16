568b87e24bc4e5f88686f407b6cd57321c77de25
Jackson Silva Lopes <ruthjack35@gmail.com>
2025-09-16 00:32:54 -0300

    first commit

---
+++ src/components/CatalogCard/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import { ProductDTO } from '../../models/product'
+import './styles.css'
+
+type Props ={
+
+  product: ProductDTO
+
+}
+
+
+export default function CatalogCard({product}: Props){
+    return (
+      <Link to={`/product-details/${product.id}`}  >
+      <div className="dsc-card">
+            <div className="dsc-catalog-card-top dsc-line-bottom">
+            <img src={product.imgUrl} alt="Computador" />
+            </div>
+            <div className="dsc-catalog-card-bottom">
+            <h3>R$ {product.price}</h3>
+              <h4>
+              {product.name}
+              </h4>
+            
+            </div>
+          </div>
+          </Link>
+    )
+}
+
