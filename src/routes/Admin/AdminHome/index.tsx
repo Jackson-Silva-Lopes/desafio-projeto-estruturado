988b6ce0fdf25a6f112fecc36fb944c1bfea99f7
Jackson Silva Lopes <ruthjack35@gmail.com>
2025-10-09 23:45:26 -0300

    Getting user

--- src/routes/Admin/AdminHome/index.tsx
+++ src/routes/Admin/AdminHome/index.tsx
@@ -1,10 +1,27 @@
 
+import { useEffect, useState } from "react";
 import "./styles.css";
+import { UserDto } from "../../../models/user";
+import * as userService from "../../../services/user-services";
 
 export default function AdminHome() {
+
+    const [user, setUser] = useState<UserDto>()
+
+    useEffect(() => {
+     userService.findMe().then(response => {
+        setUser(response.data)
+        console.log(response.data)
+     })
+     .catch(error => {
+        console.log("Error ao buscar usuário", error)
+     })
+    }, [])
+    
+
   return (
     <section id="admin-home-section" className="dsc-container">
-        <h2 className="dsc-section-title dsc-mb20">Bem-vindo à àrea administrativa</h2>
+        <h2 className="dsc-section-title dsc-mb20">Bem-vindo à àrea administrativa {user?.name}</h2>
       </section>
   )
 }
